ebc9ab59be3aa993d782da5a58d1dd624952e225
Lester702 <loganmaderia@gmail.com>
2021-06-09 12:31:29 -0400

    Trying to reset array

--- Visual Sorter/source/main.cpp
+++ Visual Sorter/source/main.cpp
@@ -3,17 +3,20 @@
 #include <fstream>
 #include <ctype.h>
 #include <string>
-#include <Windows.h>
+//#include <Windows.h>
 #include <iostream>
+#include <vector>
+
 int x = 0;
 int i = 0;
-int line = 0;
-int array[768];
+int array[2000];
 int j;
 int temp;
 
 int main(int argc, char* argv[])
 {
+	//HWND hWnd = GetConsoleWindow();
+	//ShowWindow(hWnd, SW_HIDE);
 
 	//Key press surfaces constants
 	enum KeyPressSurfaces
@@ -33,6 +36,12 @@ int main(int argc, char* argv[])
 		i++;
 	}
 
+	int n = sizeof(array) / sizeof(array[0]);
+
+	std::vector<int> vec(array, array + n);
+
+	file.close();
+
 	if (SDL_Init(SDL_INIT_VIDEO) == 0) {
 		SDL_Window* window = NULL;
 		SDL_Renderer* renderer = NULL;
@@ -40,7 +49,7 @@ int main(int argc, char* argv[])
 
 		
 
-		if (SDL_CreateWindowAndRenderer(1450, 800, 0, &window, &renderer) == 0) {
+		if (SDL_CreateWindowAndRenderer(1850, 1000, 0, &window, &renderer) == 0) {
 			SDL_bool done = SDL_FALSE;
 			screenSurface = SDL_GetWindowSurface(window);
 
@@ -51,12 +60,12 @@ int main(int argc, char* argv[])
 				SDL_RenderClear(renderer);
 				
 
-				SDL_SetRenderDrawColor(renderer, 255, 255, 255, SDL_ALPHA_OPAQUE);
+				SDL_SetRenderDrawColor(renderer, 0, 125, 242, SDL_ALPHA_OPAQUE);
 
 				x = 0;
 				for (int number : array) {
-					SDL_RenderDrawLine(renderer, x, 1050, x, 1050 - (number * 2));
-					x += 5;
+					SDL_RenderDrawLine(renderer, x, 1000, x, 1000 - (number * 0.5));
+					x += 1;
 
 				}
 
@@ -64,24 +73,56 @@ int main(int argc, char* argv[])
 
 				SDL_RenderPresent(renderer);
 
-				while (SDL_PollEvent(&event)) {
-					if (event.type == SDL_QUIT) {
+				while (SDL_PollEvent(&event) != 0)
+				{
+					//User requests quit
+					if (event.type == SDL_QUIT)
+					{
 						done = SDL_TRUE;
 					}
-					else if (event.type == SDL_KEYDOWN) {
-						for (i = 0; i < 768; i++) {
-							for (j = i + 1; j < 768; j++)
-							{
-								if (array[j] < array[i]) {
-									temp = array[i];
-									array[i] = array[j];
-									array[j] = temp;
+				}
 
-								}
+				//Set texture based on current keystate
+				const Uint8* currentKeyStates = SDL_GetKeyboardState(NULL);
+				if (currentKeyStates[SDL_SCANCODE_SPACE])
+				{
+					printf("Space was pressed\n");
+					printf("%i", vec.size());
+
+					for (i = 0; i < 2000; i++) {
+						for (j = i + 1; j < 2000; j++)
+						{
+							if (array[j] < array[i]) {
+								temp = array[i];
+								array[i] = array[j];
+								array[j] = temp;
 							}
 						}
 					}
+
+					//for (i = 0; i < 2000; i++) {
+					//	for (j = i + 1; j < 2000; j++)
+					//	{
+					//		if (array[j] < array[i]) {
+					//			temp = array[i];
+					//			array[i] = array[j];
+					//			array[j] = temp;
+					//		}
+					//	}
+					//}
 				}
+				else if (currentKeyStates[SDL_SCANCODE_R])
+				{
+					std::fill_n(array, i, 0);
+					file.open("list.txt");
+					i = 0;
+					while (file >> temp)
+					{
+						array[i] = temp;
+						i++;
+					}
+				}
+				
 			}
 		}
 
@@ -95,47 +136,3 @@ int main(int argc, char* argv[])
 	SDL_Quit();
 	return 0;
 }
-
-//int main(int argc, char* args[])
-//{
-//	//Start up SDL and create window
-//	if (!init())
-//	{
-//		printf("Failed to initialize!\n");
-//	}
-//	else
-//	{
-//		//Load media
-//		{
-//			//Main loop flag
-//			bool quit = false;
-//
-//			//Event handler
-//			
-//
-//			//While application is running
-//			while (!quit)
-//			{
-//				//Handle events on queue
-//				while (SDL_PollEvent(&e) != 0)
-//				{
-//					//User requests quit
-//					if (e.type == SDL_QUIT)
-//					{
-//						quit = true;
-//					}
-//				}
-//
-//				SDL_RenderDrawLine()
-//
-//				//Update the surface
-//				SDL_UpdateWindowSurface(gWindow);
-//			}
-//		}
-//	}
-//
-//	//Free resources and close SDL
-//	close();
-//
-//	return 0;
-//}
